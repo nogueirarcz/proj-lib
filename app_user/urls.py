baf117606095e172b0dcf6a387ff9d1459f0fe19
Guilherme Nogueira <68076593+nogueirarcz@users.noreply.github.com>
2024-09-30 16:53:25 -0300

    Criando template de login

--- app_user/urls.py
+++ app_user/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name = 'login'),
     path('cadastro/', views.cadastro, name = 'cadastro'),
-    path('validar_cadastro/', views.validar_cadastro, name='validar_cadastro'),
+    path('validar_cadastro/', views.validar_cadastro, name = 'validar_cadastro'),
+    path('validar_login/', views.validar_login, name = 'validar_login'),
 ]
