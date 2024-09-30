3843c350b65c7ace5b65cde3c1f90c1a33e26ead
Guilherme Nogueira <68076593+nogueirarcz@users.noreply.github.com>
2024-09-30 17:46:36 -0300

    Identificando usuário após session e deixando mensagem de boas vindas

--- app_livro/views.py
+++ app_livro/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from app_user.models import User
 
 # Create your views here.
 def home(request):
 
-    return HttpResponse('Homepage após login')
+    if request.session.get('user'):
+
+        user = User.objects.get(id = request.session['user']).nome
+
+        return HttpResponse(f'Bem-vindo(a) {user}')
+
+    else:
+
+        return redirect('/auth/login/?status=2')
+    
